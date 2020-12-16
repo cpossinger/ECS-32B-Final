98d402fe99a14e238dd8dc3c17f976be92604caa
cpossinger <cposs2000@gmail.com>
2020-12-15 17:07:19 -0800

    adding new files for part 2

---
+++ part2Task1.py
@@ -0,0 +1,19 @@
+"""
+BFS
+"""
+def bfs(map, office):
+
+
+
+"""
+DFS
+"""
+def dfs(map, office):
+
+
+
+
+"""
+Dijkstra's
+"""
+def dijkstra(map, office):
---
+++ part2Task2.py
@@ -0,0 +1,53 @@
+"""
+Import your Package and Truck classes at the beginning
+"""
+
+
+
+
+
+"""
+deliveryService
+"""
+def deliveryService(map, truck, packages):
+    deliveredTo = {}
+    stops = []
+
+    # write your code here
+
+    return (deliveredTo, stops)
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
